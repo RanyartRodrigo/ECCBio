399d07a59fae6a57e0e54a1ab26f2cf2e029a6a5
Gerardo Alberto González Bautista <albertoglezba@gmail.com>
2020-06-11 21:12:07 -0500

    borrando ese par de archivos

--- Panel/host.php
+++
@@ -1,6 +0,0 @@
-<?php
-	//Host apunto hacia la carpeta raiz del panel
-	$host="http://www.wegp.unam.mx/admin/Conabio2/Panel";
-	//appRoot apunta hacia la raiz del proyecto
-	$appRoot="https://";
-?>
