7e9ae3d61f11bfea0b3a33e8b078c428cd349567
Gerardo Alberto González Bautista <albertoglezba@gmail.com>
2020-06-26 16:50:04 -0500

    script para crear tabla

---
+++ dbScripts/crea_colores.sql
@@ -0,0 +1,6 @@
+CREATE TABLE colores(id INT AUTO_INCREMENT, color VARCHAR(50), PRIMARY KEY(id));
+INSERT INTO colores (color) VALUES ('rgba(156, 96, 61, 1)');
+INSERT INTO colores (color) VALUES ('rgba(245, 246, 244, 1)');
+INSERT INTO colores (color) VALUES ('rgba(123, 163, 180, 1)');
+INSERT INTO colores (color) VALUES ('rgba(52, 48, 58, 1)');
+INSERT INTO colores (color) VALUES ('rgba(207, 170, 54, 1)');
